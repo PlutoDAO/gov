cde907545af4fb76cf5599442ee645dd0e8cf5a0
Hernán Peralta <alegodemencia@gmail.com>
2021-10-25 17:03:52 -0300

    Add unit tests for whitelisted assets functionality
    Also included some minor refactorings.

--- Answap.Gov.Test/Domain/ProposalTest.cs
+++ Answap.Gov.Test/Domain/ProposalTest.cs
@@ -16,11 +16,12 @@ namespace Answap.Gov.Test.Domain
                 ProposalHelper.GetDescription(),
                 ProposalHelper.GetFakeCreator(),
                 ProposalHelper.GetCreationDate(),
-                ProposalHelper.GetDeadline()
+                ProposalHelper.GetDeadline(),
+                WhitelistedAssetHelper.GetWhitelistedAssets()
             );
 
             var invalidOption = new Option("NO");
-            var invalidVote = new Vote(VoteHelper.GetFakeVoter(), invalidOption, VoteHelper.GetFakeToken(), 1000);
+            var invalidVote = new Vote("FakeVoter", invalidOption, AssetHelper.GetFakeAsset(), 1000);
 
             Assert.Throws<InvalidOptionException>(() => proposal.CastVote(invalidVote));
         }
@@ -33,7 +34,8 @@ namespace Answap.Gov.Test.Domain
                 ProposalHelper.GetDescription(),
                 ProposalHelper.GetFakeCreator(),
                 DateTime.Now.AddMinutes(1),
-                DateTime.Now
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
             );
             
             Assert.False(proposal.IsVoteClosed());
@@ -47,9 +49,11 @@ namespace Answap.Gov.Test.Domain
                 ProposalHelper.GetDescription(),
                 ProposalHelper.GetFakeCreator(),
                 DateTime.Now.AddMinutes(1),
-                DateTime.Now
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
             );
-            var vote = new Vote("FakeVoter", new Option("FOR"), "FakeToken", 1);
+            var ani = AssetHelper.GetAni();
+            var vote = new Vote("FakeVoter", new Option("FOR"), ani, 1);
             var validatedVote = proposal.CastVote(vote);
 
             Assert.IsType<ValidatedVote>(validatedVote);
@@ -63,27 +67,70 @@ namespace Answap.Gov.Test.Domain
                 ProposalHelper.GetDescription(),
                 ProposalHelper.GetFakeCreator(),
                 DateTime.Now.AddMinutes(-1),
-                DateTime.Now
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
             );
             
             Assert.True(proposal.IsVoteClosed());
         }
 
         [Fact]
+        public void TestAssetNotWhitelistedThrowsError()
+        {
+            var proposal = new Proposal(
+                ProposalHelper.GetName(),
+                ProposalHelper.GetDescription(),
+                ProposalHelper.GetFakeCreator(),
+                DateTime.Now.AddMinutes(1),
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
+            );
+            var fakeAsset = AssetHelper.GetFakeAsset();
+            var vote = new Vote("FakeVoter", new Option("FOR"), fakeAsset, 1);
+
+            Assert.Throws<NotWhitelistedAssetException>(() => proposal.CastVote(vote));
+        }
+
+        [Fact]
         public void TestDeclareWinnerFindsTheWinnerOption()
         {
-            var vote1 = new Vote("voter1", new Option("FOR"), "token1", 1);
-            var vote2 = new Vote("voter2", new Option("FOR"), "token2", 2);
-            var vote3 = new Vote("voter3", new Option("AGAINST"), "token3", 2);
+            var ani = AssetHelper.GetAni();
+            var validatedVote1 = new ValidatedVote("voter1", new Option("FOR"), ani, 1);
+            var validatedVote2 = new ValidatedVote("voter2", new Option("FOR"), ani, 2);
+            var validatedVote3 = new ValidatedVote("voter3", new Option("AGAINST"), ani, 2);
             var proposal = new Proposal(
                 ProposalHelper.GetName(),
                 ProposalHelper.GetDescription(),
                 ProposalHelper.GetFakeCreator(),
                 DateTime.Now.AddMinutes(-1),
-                DateTime.Now
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
             );
 
-            var winningOption = proposal.DeclareWinner(new[] {vote1, vote2, vote3});
+            var winningOption = proposal.DeclareWinner(new[] {validatedVote1, validatedVote2, validatedVote3});
+            
+            Assert.Equal("FOR", winningOption.Name);
+        }
+
+        [Fact]
+        public void TestDeclareWinnerAllowsWhitelistedAssets()
+        {
+            var ani = AssetHelper.GetAni();
+            var ars = AssetHelper.GetArs();
+            var usdc = AssetHelper.GetUsdc();
+            var validatedVote1 = new ValidatedVote("voter1", new Option("FOR"), ani, 2);
+            var validatedVote2 = new ValidatedVote("voter2", new Option("AGAINST"), ars, 2);
+            var validatedVote3 = new ValidatedVote("voter3", new Option("AGAINST"), usdc, 2);
+            var proposal = new Proposal(
+                ProposalHelper.GetName(),
+                ProposalHelper.GetDescription(),
+                ProposalHelper.GetFakeCreator(),
+                DateTime.Now.AddMinutes(1),
+                DateTime.Now,
+                WhitelistedAssetHelper.GetWhitelistedAssets()
+            );
+            
+            var winningOption = proposal.DeclareWinner(new[] {validatedVote1, validatedVote2, validatedVote3});
             
             Assert.Equal("FOR", winningOption.Name);
         }
