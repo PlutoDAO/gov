fa3f350f91e9a22ef63a3a646516dd9f4d768420
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2021-11-02 07:31:03 +1100

    Adapt Mappers to use the new WhitelistedAsset implementation

--- PlutoDAO.Gov.Application/Proposals/Mappers/ProposalMapper.cs
+++ PlutoDAO.Gov.Application/Proposals/Mappers/ProposalMapper.cs
@@ -1,21 +1,22 @@
+using System.Linq;
 using PlutoDAO.Gov.Application.Proposals.Responses;
 using PlutoDAO.Gov.Application.WhitelistedAssets;
 using PlutoDAO.Gov.Domain;
 
 namespace PlutoDAO.Gov.Application.Proposals.Mappers
 {
-    public class ProposalMapper
+    public static class ProposalMapper
     {
         public static IProposalResponse Map(Proposal proposal)
         {
             return new ProposalResponse
             {
-             Name = proposal.Name,
-             Description = proposal.Description,
-             Creator = proposal.Creator,
-             Deadline = proposal.Deadline,
-             Created = proposal.Created,
-             WhitelistedAssets = WhitelistedAssetMapper.Map(proposal.WhitelistedAssets)
+                Name = proposal.Name,
+                Description = proposal.Description,
+                Creator = proposal.Creator,
+                Deadline = proposal.Deadline,
+                Created = proposal.Created,
+                WhitelistedAssets = proposal.WhitelistedAssets.Select(WhitelistedAssetMapper.Map)
             };
         }
     }
--- PlutoDAO.Gov.Application/Proposals/Responses/IProposalResponse.cs
+++ PlutoDAO.Gov.Application/Proposals/Responses/IProposalResponse.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Collections.Generic;
 using PlutoDAO.Gov.Application.WhitelistedAssets;
 
 namespace PlutoDAO.Gov.Application.Proposals.Responses
 {
     public interface IProposalResponse
     {
-        public string Name {get; set;}
-        public string Description {get; set;}
-        public string Creator {get; set;}
-        public DateTime Deadline {get; set;}
-        public DateTime Created {get; set;}
-        public WhitelistedAssetDto WhitelistedAssets {get; set;}
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Creator { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime Created { get; set; }
+        public IEnumerable<WhitelistedAssetDto> WhitelistedAssets { get; set; }
     }
 }
