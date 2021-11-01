f4f1bb5f7ae295b68c47fd6c58fe7c1a296620dd
Hernán Peralta <alegodemencia@gmail.com>
2021-11-01 19:00:44 -0300

    Refactor GetWhitelistedAssets() helper method signature

--- PlutoDAO.Gov.Test/Helpers/WhitelistedAssetHelper.cs
+++ PlutoDAO.Gov.Test/Helpers/WhitelistedAssetHelper.cs
@@ -1,15 +1,16 @@
+using System.Collections.Generic;
 using PlutoDAO.Gov.Domain;
 
 namespace PlutoDAO.Gov.Test.Helpers
 {
     public class WhitelistedAssetHelper
     {
-        public static WhitelistedAsset GetWhitelistedAssets()
+        public static IEnumerable<WhitelistedAsset> GetWhitelistedAssets()
         {
             var plt = AssetHelper.GetPlt();
             var ars = AssetHelper.GetArs();
             var usdc = AssetHelper.GetUsdc();
-            return new WhitelistedAsset(){{plt, 2}, {usdc, 1}, {ars, 0.5m}};
+            return new List<WhitelistedAsset>{new (plt, 2m), new (ars, 0.5m), new (usdc, 1m)};
         }   
     }
 }
