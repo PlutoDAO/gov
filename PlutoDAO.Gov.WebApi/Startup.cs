e7b4d1e037fce6dedcc2b796466652fc732798da
Hernán Peralta <alegodemencia@gmail.com>
2021-11-01 16:13:10 -0300

    Add dependency injection configuration for proposal service and repository layers

--- PlutoDAO.Gov.WebApi/Startup.cs
+++ PlutoDAO.Gov.WebApi/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlutoDAO.Gov.Application.Proposals;
+using PlutoDAO.Gov.Infrastructure.Stellar.Proposals;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +28,9 @@ namespace PlutoDAO.Gov.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ProposalService>();
+            services.AddScoped<IProposalRepository, ProposalRepository>();
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
