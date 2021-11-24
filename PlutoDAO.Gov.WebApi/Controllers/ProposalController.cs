67fb5e6fe483efaec6aa53211dd1da06ac80a9ca
Hernán Peralta <alegodemencia@gmail.com>
2021-11-24 12:05:10 -0300

    Add save and get proposals endpoints to ProposalController

--- PlutoDAO.Gov.WebApi/Controllers/ProposalController.cs
+++ PlutoDAO.Gov.WebApi/Controllers/ProposalController.cs
@@ -5,6 +5,7 @@ using PlutoDAO.Gov.Application.Proposals;
 using PlutoDAO.Gov.Application.Proposals.Responses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PlutoDAO.Gov.WebApi.Request;
 
 namespace PlutoDAO.Gov.WebApi.Controllers
 {
@@ -30,6 +31,36 @@ namespace PlutoDAO.Gov.WebApi.Controllers
             catch (ProposalNotFoundException e)
             {
                 return Problem(e.Message, null, 404, e.Title, e.Type);
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                return Ok(await _proposalService.GetAll());
+
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Save(ProposalRequest request)
+        {
+            try
+            {
+                await _proposalService.Save(request);
+                return Ok();
             }
             catch (Exception e)
             {
