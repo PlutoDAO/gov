e850f55848a68cdc1b37de2905728925bf64173e
Hernán Peralta <alegodemencia@gmail.com>
2021-11-24 12:05:55 -0300

    Add Save() and GetAll() methods to ProposalService

--- PlutoDAO.Gov.Application/Proposals/ProposalService.cs
+++ PlutoDAO.Gov.Application/Proposals/ProposalService.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using PlutoDAO.Gov.Application.Exceptions;
 using PlutoDAO.Gov.Application.Proposals.Mappers;
+using PlutoDAO.Gov.Application.Proposals.Requests;
 using PlutoDAO.Gov.Application.Proposals.Responses;
+using PlutoDAO.Gov.Domain;
 
 namespace PlutoDAO.Gov.Application.Proposals
 {
@@ -25,6 +30,23 @@ namespace PlutoDAO.Gov.Application.Proposals
             }
 
             return ProposalMapper.Map(proposal);
+        }
+
+        public async Task Save(IProposalRequest request)
+        {
+            var proposal = new Proposal(request.Name,
+                request.Description,
+                request.Creator,
+                DateTime.Parse(request.Deadline),
+                DateTime.Now,
+                new List<WhitelistedAsset>(request.WhitelistedAssets.Select(w => (WhitelistedAsset)w)));
+            await _proposalRepository.SaveProposal(proposal);
+        }
+
+        public async Task<IProposalResponse[]> GetAll()
+        {
+            var proposals = await _proposalRepository.GetProposals();
+            return proposals.Select(ProposalMapper.Map).ToArray();
         }
     }
 }
