ebfaf3d55555dd65db702cd7055a00974cb4f98e
Hernán Peralta <alegodemencia@gmail.com>
2021-11-24 10:55:11 -0300

    Add integration tests fixtures

---
+++ PlutoDAO.Gov.Test.Integration/Fixtures/StellarCollection.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace PlutoDAO.Gov.Test.Integration.Fixtures
+{
+    [CollectionDefinition("Stellar collection")]
+    public class StellarCollection : ICollectionFixture<StellarFixture>
+    {
+    }
+}
