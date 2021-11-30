87d7a02e681257c766ae60d259c470d1acc1b8d7
Hernán Peralta <alegodemencia@gmail.com>
2021-11-30 18:00:02 -0300

    Add test to Stellar collection

--- PlutoDAO.Gov.Test.Integration/Controllers/_1GetProposalTestController.cs
+++ PlutoDAO.Gov.Test.Integration/Controllers/_1GetProposalTestController.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using PlutoDAO.Gov.Test.Integration.Fixtures;
 using PlutoDAO.Gov.Test.Integration.Helpers;
 using PlutoDAO.Gov.WebApi;
 using Xunit;
@@ -8,16 +9,23 @@ using Xunit.Abstractions;
 
 namespace PlutoDAO.Gov.Test.Integration.Controllers
 {
+    [Collection("Stellar collection")]
+    [TestCaseOrderer("PlutoDAO.Gov.Test.Integration.Helpers.AlphabeticalOrderer", "PlutoDAO.Gov.Test.Integration")]
     public class _1GetProposalTestController : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
         private readonly ITestOutputHelper _testOutputHelper;
 
-        public _1GetProposalTestController(WebApplicationFactory<Startup> factory, ITestOutputHelper testOutputHelper)
+        public _1GetProposalTestController(WebApplicationFactory<Startup> factory,
+            StellarFixture fixture,
+            ITestOutputHelper testOutputHelper)
         {
             _factory = factory;
+            Config = fixture.Config;
             _testOutputHelper = testOutputHelper;
         }
+        
+        private TestConfiguration Config { get; }
 
         [Fact]
         public async Task Test_00_Get_Proposal()
