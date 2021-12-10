ac7f2396c262b6dc2922d06e2fc795d66309ea40
Hernán Peralta <alegodemencia@gmail.com>
2021-12-10 16:10:26 -0300

    Create ProposalIdentifier response

---
+++ PlutoDAO.Gov.Application/Proposals/Responses/IProposalIdentifier.cs
@@ -0,0 +1,8 @@
+namespace PlutoDAO.Gov.Application.Proposals.Responses
+{
+    public interface IProposalIdentifier
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PlutoDAO.Gov.Application/Proposals/Responses/ProposalIdentifier.cs
@@ -0,0 +1,8 @@
+namespace PlutoDAO.Gov.Application.Proposals.Responses
+{
+    public class ProposalIdentifier : IProposalIdentifier
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
