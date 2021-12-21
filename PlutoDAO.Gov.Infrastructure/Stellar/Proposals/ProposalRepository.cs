22b07906845183a92e7ca9fadb43ad5b8f820ae6
Hernán Peralta <alegodemencia@gmail.com>
2021-12-21 11:21:49 -0300

    Add functionality for saving more than 638 characters in a proposal

--- PlutoDAO.Gov.Infrastructure/Stellar/Proposals/ProposalRepository.cs
+++ PlutoDAO.Gov.Infrastructure/Stellar/Proposals/ProposalRepository.cs
@@ -11,13 +11,11 @@ using PlutoDAO.Gov.Infrastructure.Stellar.Helpers;
 using stellar_dotnet_sdk;
 using stellar_dotnet_sdk.responses.operations;
 using Asset = stellar_dotnet_sdk.Asset;
-using DomainAsset = PlutoDAO.Gov.Domain.Asset;
 
 namespace PlutoDAO.Gov.Infrastructure.Stellar.Proposals
 {
     public class ProposalRepository : IProposalRepository
     {
-        private const string AssetCode = "PROPCOIN1";
         private readonly Server _server;
         private readonly SystemAccountConfiguration _systemAccountConfiguration;
 
@@ -27,23 +25,9 @@ namespace PlutoDAO.Gov.Infrastructure.Stellar.Proposals
             _server = server;
         }
 
-        public async Task<Proposal> FindProposal(string address)
-        {
-            return new Proposal(
-                "name",
-                "description",
-                "creator",
-                DateTime.Now.AddHours(1),
-                DateTime.Today,
-                new[]
-                {
-                    new WhitelistedAsset(new DomainAsset(new AccountAddress("STELLAR"), "XLM", true), 1)
-                }
-            );
-        }
-
         public async Task SaveProposal(Proposal proposal)
         {
+            const int maximumProposalLength = 637;
             if (proposal.Name.Length > EncodingHelper.MemoTextMaximumCharacters)
                 throw new ArgumentOutOfRangeException("The proposal name cannot exceed 28 characters");
 
@@ -52,22 +36,109 @@ namespace PlutoDAO.Gov.Infrastructure.Stellar.Proposals
                 ReferenceLoopHandling = ReferenceLoopHandling.Serialize
             });
 
-            var encodedProposalPayments = EncodingHelper.Encode(serializedProposal);
-
             var proposalSenderKeyPair = KeyPair.FromSecretSeed(_systemAccountConfiguration.SenderPrivateKey);
             var proposalReceiverKeyPair = KeyPair.FromSecretSeed(_systemAccountConfiguration.ReceiverPrivateKey);
             var senderAccountResponse = await _server.Accounts.Account(proposalSenderKeyPair.AccountId);
             var senderAccount = new Account(proposalSenderKeyPair.AccountId, senderAccountResponse.SequenceNumber);
+            var assetCode = await GenerateAssetCode(proposalReceiverKeyPair.AccountId, proposalSenderKeyPair.AccountId);
+
+            if (serializedProposal.Length <= maximumProposalLength)
+                await SaveProposal(serializedProposal, 
+                    proposal.Name, 
+                    assetCode, 
+                    proposalSenderKeyPair, 
+                    proposalReceiverKeyPair, 
+                    senderAccount);
+            else
+                for (var i = 0; i < serializedProposal.Length; i += maximumProposalLength)
+                {
+                    var serializedProposalSection = serializedProposal.Substring(i,
+                        serializedProposal.Length - i > maximumProposalLength
+                            ? maximumProposalLength
+                            : serializedProposal.Length - i);
+                    await SaveProposal(
+                        serializedProposalSection, 
+                        proposal.Name, 
+                        assetCode, 
+                        proposalSenderKeyPair, 
+                        proposalReceiverKeyPair, 
+                        senderAccount);
+                }
+        }
+
+        public async Task<Proposal> GetProposal(string assetCode)
+        {
+            IList<object> transactionHashesAll = new List<object>();
+            var proposalReceiverKeyPair = KeyPair.FromSecretSeed(_systemAccountConfiguration.ReceiverPrivateKey);
+            IList<PaymentOperationResponse> retrievedRecords = new List<PaymentOperationResponse>();
+
+            var response =
+                await _server.Payments.ForAccount(proposalReceiverKeyPair.AccountId).Limit(200).Execute();
+            while (response.Embedded.Records.Count != 0)
+            {
+                var paymentRecords = response.Records.OfType<PaymentOperationResponse>()
+                    .Where(payment => payment.TransactionSuccessful).ToList();
+
+                foreach (var record in paymentRecords)
+                    if (record.AssetCode == assetCode && record.To == proposalReceiverKeyPair.AccountId)
+                    {
+                        retrievedRecords.Add(record);
+                        transactionHashesAll.Add(record.TransactionHash);
+                    }
+
+                response = await response.NextPage();
+            }
+
+            var decodedProposal = EncodingHelper.Decode(retrievedRecords, transactionHashesAll);
+            return JsonConvert.DeserializeObject<Proposal>(decodedProposal);
+        }
+
+        public async Task<ProposalIdentifier[]> GetProposalList()
+        {
+            IList<ProposalIdentifier> proposalList = new List<ProposalIdentifier>();
+            var proposalReceiverPublicKey =
+                KeyPair.FromSecretSeed(_systemAccountConfiguration.ReceiverPrivateKey).AccountId;
+            var proposalSenderPublicKey =
+                KeyPair.FromSecretSeed(_systemAccountConfiguration.SenderPrivateKey).AccountId;
+
+            var response =
+                await _server.Transactions.ForAccount(proposalReceiverPublicKey).Limit(200).Execute();
+            var transactionRecords = response.Records
+                .Where(transaction => transaction.SourceAccount == proposalSenderPublicKey);
+
+            foreach (var record in transactionRecords)
+                if (proposalList.All(identifier => identifier.Name != record.MemoValue))
+                {
+                    var assetCode = (await _server.Payments.ForTransaction(record.Hash)
+                            .Execute()).Records.OfType<PaymentOperationResponse>()
+                        .First()
+                        .AssetCode;
+
+                    proposalList.Add(new ProposalIdentifier {Id = assetCode, Name = record.MemoValue});
+                }
+
+            return proposalList.ToArray();
+        }
+
+        private async Task SaveProposal(
+            string serializedProposalSection, 
+            string proposalName, 
+            string assetCode, 
+            KeyPair proposalSenderKeyPair, 
+            KeyPair proposalReceiverKeyPair, 
+            Account senderAccount)
+        {
+            var encodedProposalPayments = EncodingHelper.Encode(serializedProposalSection);
 
             var txBuilder = new TransactionBuilder(senderAccount);
-            var asset = Asset.CreateNonNativeAsset(AssetCode, proposalReceiverKeyPair.AccountId);
+            var asset = Asset.CreateNonNativeAsset(assetCode, proposalReceiverKeyPair.AccountId);
 
             var changeTrustLineOp = new ChangeTrustOperation.Builder(ChangeTrustAsset.Create(asset))
                 .SetSourceAccount(senderAccount.KeyPair).Build();
             var paymentOp =
                 new PaymentOperation.Builder(proposalSenderKeyPair, asset, EncodingHelper.MaxTokens.ToString())
                     .SetSourceAccount(proposalReceiverKeyPair).Build();
-            txBuilder.AddOperation(changeTrustLineOp).AddOperation(paymentOp).AddMemo(new MemoText(proposal.Name));
+            txBuilder.AddOperation(changeTrustLineOp).AddOperation(paymentOp).AddMemo(new MemoText(proposalName));
 
             foreach (var payment in encodedProposalPayments.EncodedProposalMicropayments)
             {
@@ -100,52 +171,25 @@ namespace PlutoDAO.Gov.Infrastructure.Stellar.Proposals
                 );
         }
 
-        public async Task<Proposal[]> GetProposals()
+        private async Task<string> GenerateAssetCode(string proposalReceiverPublicKey, string proposalSenderAccount)
         {
-            IList<object> transactionHashesAll = new List<object>();
-            var proposalReceiverKeyPair = KeyPair.FromSecretSeed(_systemAccountConfiguration.ReceiverPrivateKey);
-            IList<PaymentOperationResponse> retrievedRecords = new List<PaymentOperationResponse>();
-
+            IList<string> assetList = new List<string>();
             var response =
-                await _server.Payments.ForAccount(proposalReceiverKeyPair.AccountId).Limit(200).Execute();
-            var paymentRecords = response.Records.OfType<PaymentOperationResponse>()
-                .Where(payment => payment.TransactionSuccessful).ToList();
-
-            foreach (var record in paymentRecords)
-                if (record.AssetCode == AssetCode && record.To == proposalReceiverKeyPair.AccountId)
+                await _server.Payments.ForAccount(proposalSenderAccount).Limit(200).Execute();
+            while (response.Embedded.Records.Count != 0)
+            {
+                foreach (var payment in response.Records.OfType<PaymentOperationResponse>())
                 {
-                    retrievedRecords.Add(record);
-                    transactionHashesAll.Add(record.TransactionHash);
+                    if (payment.SourceAccount == proposalReceiverPublicKey && payment.AssetCode.Contains("PROP"))
+                    {
+                        assetList.Add(payment.AssetCode);
+                    }
                 }
+                response = await response.NextPage();
+            }
 
-            return EncodingHelper.Decode(retrievedRecords, transactionHashesAll);
-        }
-
-        public async Task<ProposalIdentifier[]> GetProposalList()
-        {
-            IList<ProposalIdentifier> proposalList = new List<ProposalIdentifier>();
-            var proposalReceiverPublicKey =
-                KeyPair.FromSecretSeed(_systemAccountConfiguration.ReceiverPrivateKey).AccountId;
-            var proposalSenderPublicKey =
-                KeyPair.FromSecretSeed(_systemAccountConfiguration.SenderPrivateKey).AccountId;
-
-            var response =
-                await _server.Transactions.ForAccount(proposalReceiverPublicKey).Limit(200).Execute();
-            var transactionRecords = response.Records
-                .Where(transaction => transaction.SourceAccount == proposalSenderPublicKey);
-
-            foreach (var record in transactionRecords)
-                if (proposalList.All(identifier => identifier.Name != record.MemoValue))
-                {
-                    var assetCode = (await _server.Payments.ForTransaction(record.Hash)
-                            .Execute()).Records.OfType<PaymentOperationResponse>()
-                        .First()
-                        .AssetCode;
-
-                    proposalList.Add(new ProposalIdentifier {Id = AssetCode, Name = record.MemoValue});
-                }
-
-            return proposalList.ToArray();
+            var uniqueAssetCount = assetList.Distinct().Count();
+            return $"PROP{uniqueAssetCount + 1}";
         }
     }
 }
