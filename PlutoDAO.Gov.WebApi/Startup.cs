72b308d42bc00211871ccbc6e82053d9ec80acb1
Hernán Peralta <alegodemencia@gmail.com>
2022-02-01 09:36:05 -0300

    Register dependencies and environment variables at Startup.cs

--- PlutoDAO.Gov.WebApi/Startup.cs
+++ PlutoDAO.Gov.WebApi/Startup.cs
@@ -6,8 +6,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using PlutoDAO.Gov.Application.Proposals;
+using PlutoDAO.Gov.Application.Votes;
 using PlutoDAO.Gov.Infrastructure.Stellar;
 using PlutoDAO.Gov.Infrastructure.Stellar.Proposals;
+using PlutoDAO.Gov.Infrastructure.Stellar.Votes;
 using stellar_dotnet_sdk;
 
 namespace PlutoDAO.Gov.WebApi
@@ -28,12 +30,16 @@ namespace PlutoDAO.Gov.WebApi
                 Environment.GetEnvironmentVariable("PLUTODAO_PROPOSAL_SENDER_ACCOUNT_PRIVATE_KEY") ??
                 throw new ApplicationException("PLUTODAO_PROPOSAL_SENDER_ACCOUNT_PRIVATE_KEY not set"),
                 Environment.GetEnvironmentVariable("PLUTODAO_PROPOSAL_RECEIVER_ACCOUNT_PRIVATE_KEY") ??
-                throw new ApplicationException("PLUTODAO_PROPOSAL_RECEIVER_ACCOUNT_PRIVATE_KEY not set")));
+                throw new ApplicationException("PLUTODAO_PROPOSAL_RECEIVER_ACCOUNT_PRIVATE_KEY not set"),
+                Environment.GetEnvironmentVariable("PLUTODAO_ESCROW_ACCOUNT_PRIVATE_KEY") ??
+                throw new ApplicationException("PLUTODAO_ESCROW_ACCOUNT_PRIVATE_KEY not set")));
 
             services.AddScoped(_ => new Server(Environment.GetEnvironmentVariable("HORIZON_URL")));
 
             services.AddScoped<ProposalService>();
+            services.AddScoped<VoteService>();
             services.AddScoped<IProposalRepository, ProposalRepository>();
+            services.AddScoped<IVoteRepository, VoteRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -45,13 +51,6 @@ namespace PlutoDAO.Gov.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlutoDAO.Gov.WebApi v1"));
-            }
-
             if (env.IsDevelopment())
             {
                 Network.Use(new Network(Environment.GetEnvironmentVariable("HORIZON_NETWORK_PASSPHRASE")));
