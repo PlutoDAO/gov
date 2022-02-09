c2e492a2001e8582e6675cd436fdaf8238cc5216
Hernán Peralta <alegodemencia@gmail.com>
2022-02-09 12:17:58 -0300

    Add Options mapping in ProposalMapper.cs

--- PlutoDAO.Gov.Application/Proposals/Mappers/ProposalMapper.cs
+++ PlutoDAO.Gov.Application/Proposals/Mappers/ProposalMapper.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using PlutoDAO.Gov.Application.Options;
 using PlutoDAO.Gov.Application.Proposals.Responses;
 using PlutoDAO.Gov.Application.WhitelistedAssets;
 using PlutoDAO.Gov.Domain;
@@ -16,7 +17,8 @@ namespace PlutoDAO.Gov.Application.Proposals.Mappers
                 Creator = proposal.Creator,
                 Deadline = proposal.Deadline,
                 Created = proposal.Created,
-                WhitelistedAssets = proposal.WhitelistedAssets.Select(WhitelistedAssetMapper.Map)
+                WhitelistedAssets = proposal.WhitelistedAssets.Select(WhitelistedAssetMapper.Map),
+                Options = proposal.Options.Select(option => (OptionDto)option)
             };
         }
     }
