3628524022d4cb5e80cc80df6ecc98ec8c9986c3
Hernán Peralta <alegodemencia@gmail.com>
2022-03-02 14:52:02 -0300

    Remove no longer valid assert in EncondingHelperTest

--- PlutoDAO.Gov.Test/Infrastructure/Stellar/Helpers/EncodingHelperTest.cs
+++ PlutoDAO.Gov.Test/Infrastructure/Stellar/Helpers/EncodingHelperTest.cs
@@ -63,7 +63,6 @@ namespace PlutoDAO.Gov.Test.Infrastructure.Stellar.Helpers
         public void TestEncodeSequenceNumberToBase48()
         {
             Assert.Equal("4qXPrAFC", EncodingHelper.EncodeSeqNumberToBase48(91581235487423215));
-            Assert.Throws<ArgumentOutOfRangeException>(() => EncodingHelper.EncodeSeqNumberToBase48(123));
         }
     }
 }
