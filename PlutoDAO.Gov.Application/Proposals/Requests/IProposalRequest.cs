e1b83ec4f3ad8b14acf832106ad7671b0e24cbe2
Hernán Peralta <alegodemencia@gmail.com>
2022-05-27 20:01:03 -0300

    Add AssetHelper class to centralize asset creation

--- PlutoDAO.Gov.Application/Proposals/Requests/IProposalRequest.cs
+++ PlutoDAO.Gov.Application/Proposals/Requests/IProposalRequest.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using PlutoDAO.Gov.Application.WhitelistedAssets;
-
 namespace PlutoDAO.Gov.Application.Proposals.Requests
 {
     public interface IProposalRequest
@@ -8,6 +5,5 @@ namespace PlutoDAO.Gov.Application.Proposals.Requests
         public string Name { get; set; }
         public string Description { get; set; }
         public string Creator { get; set; }
-        public IEnumerable<WhitelistedAssetDto> WhitelistedAssets { get; set; }
     }
 }
