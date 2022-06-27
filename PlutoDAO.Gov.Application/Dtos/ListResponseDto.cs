743cba3249edfd6f93aa95507725c5a68b8cabb2
Genebson <mauryavero@gmail.com>
2022-06-27 12:21:06 -0300

    Add DTO

---
+++ PlutoDAO.Gov.Application/Dtos/ListResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using PlutoDAO.Gov.Application.Extensions;
+using PlutoDAO.Gov.Application.Proposals.Responses;
+using System.Collections.Generic;
+
+namespace PlutoDAO.Gov.Application.Dtos
+{
+    public class ListResponseDto : ILinkedResource
+    {
+        public int CurrentPage { get; init; }
+        public int TotalItems { get; init; }
+        public int TotalPages { get; init; }
+        public List<IProposalIdentifier> Items { get; init; }
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+    }
+}
