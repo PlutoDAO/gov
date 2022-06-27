867e9106bc7fe8b18d992b3b26d045f0ef933182
Genebson <mauryavero@gmail.com>
2022-06-27 12:22:04 -0300

    Add Pagination Extensions

---
+++ PlutoDAO.Gov.Application/Extensions/ILinkedResource.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PlutoDAO.Gov.Application.Extensions
+{
+    public interface ILinkedResource
+    {
+        public IDictionary<LinkedResourceType, LinkedResource> Links { get; set; }
+    }
+}
---
+++ PlutoDAO.Gov.Application/Extensions/PagerExtension.cs
@@ -0,0 +1,33 @@
+﻿using PlutoDAO.Gov.Application.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlutoDAO.Gov.Application.Extensions
+{
+    public static class PagerExtension
+    {
+        public static Task<PagedModel<TModel>> Paginate<TModel>(
+            IQueryable<TModel> query,
+            int page,
+            int limit
+        ) where TModel : class
+        {
+            var paged = new PagedModel<TModel>();
+
+            page = (page < 0) ? 1 : page;
+
+            paged.CurrentPage = page;
+            paged.PageSize = limit;
+
+            paged.TotalItems = query.Count();
+
+            var startRow = (page - 1) * limit;
+            paged.Items = query.Skip(startRow).Take(limit).ToList();
+
+            paged.TotalPages = (int)Math.Ceiling(paged.TotalItems / (double)limit);
+
+            return Task.FromResult(paged);
+        }
+    }
+}
